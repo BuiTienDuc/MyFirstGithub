e2ad53e0cd98da18921bd91b10bd850a9cda6144
tiend <tiend@192.168.56.1>
2020-07-11 15:27:42 +0700

    My first GitHub

---
+++ MyFirstGitHubEclipse/src/bui/tien/duc/MyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bui.tien.duc;
+
+/**
+ * @author tiend
+ *
+ */
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo World");
+	}
+
+}
---
+++ MyFirstGitHubEclipse/src/bui/tien/duc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author tiend
+ *
+ */
+package bui.tien.duc;
